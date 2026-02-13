5cd4202fd526662d347ab30a8d471db076bf0ab2
code-ninjax <157591552+code-ninjax@users.noreply.github.com>
2026-02-13 07:20:46 +0100

    Update TeamCard to prevent image cropping (use object-contain)

--- components/TeamCard.tsx
+++ components/TeamCard.tsx
@@ -15,13 +15,13 @@ export const TeamCard = ({ member, onClick }: { member: TeamMember; onClick?: ()
       onClick={onClick}
     >
       {/* Image Container */}
-      <div className="relative h-2/3 w-full overflow-hidden bg-gray-50">
+      <div className="relative h-2/3 w-full overflow-hidden bg-gray-100">
         {member.image_url ? (
           <Image 
             src={member.image_url} 
             alt={member.name} 
             fill 
-            className="object-cover group-hover:scale-110 transition-transform duration-[1500ms] ease-out"
+            className="object-contain p-4 group-hover:scale-105 transition-transform duration-[1500ms] ease-out"
           />
         ) : (
           <div className="w-full h-full flex items-center justify-center">
